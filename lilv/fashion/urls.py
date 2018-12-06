e58e1bb04082a338ea75f08ea51d360168fa7103
974712433 <1259123495@qq.com>
2018-12-06 13:00:24 +0800

    new

--- lilv/fashion/urls.py
+++ lilv/fashion/urls.py
@@ -3,8 +3,11 @@ from django.conf.urls import url
 from fashion import views
 
 urlpatterns = [
-    url(r'^$',views.index,name='index'),
-    url(r'^login/$',views.login,name='login'),
-    url(r'^register/$',views.register,name='register'),
-    url(r'^car/$',views.car,name='car'),
+    url(r'^$',views.index, name='index'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^exit/$',views.exit,name='exit'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^car/$', views.car, name='car'),
+    url(r'^goods/$',views.goods, name='goods'),
+    url(r'^list/$', views.list, name='list'),
 ]
--- lilv/fashion/views.py
+++ lilv/fashion/views.py
@@ -1,17 +1,71 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from fashion.models import User
+
+
 def index(request):
-    return render(request,'index.html')
+    tel = request.COOKIES.get('tel')
+    return render(request, 'index.html', context={'tel': tel})
 
 
 def login(request):
-    return render(request,'login.html')
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        tel = request.POST.get('tel')
+        password = request.POST.get('password')
+
+        users = User.objects.filter(tel=tel).filter(password=password)
+        if users.count():
+            response = redirect('fashion:index')
+            response.set_cookie('tel', tel)
+            return response
+
+        else:
+            return render(request,'login.html')
+
 
 
 def register(request):
-    return render(request,'register.html')
+    if request.method == 'GET':
+        return render(request, 'register01.html')
+    elif request.method == 'POST':
+        tel = request.POST.get('tel')
+
+        password = request.POST.get('password')
+
+
+        user = User()
+        user.tel = tel
+        user.password = password
+        user.save()
+
+        response = redirect('fashion:index')
+
+        response.set_cookie('tel',tel)
+
+        #return HttpResponse('稍等')
+        return response
+
+
+def exit(request):
+    response = redirect('fashion:index')
+    response.delete_cookie('tel')
+    return response
+
 
 
 def car(request):
-    return render(request,'car.html')
+    return render(request, 'car.html')
+
+
+def goods(request):
+    return render(request, 'goods.html')
+
+
+def list(request):
+    return render(request, 'list.html')
+
+
